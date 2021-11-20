fe1685258ca894bd8746dd0520eb3ae0c892b813
Adrian <adrianmuino27@hotmail.com>
2021-11-19 20:21:32 -0500

    Add User and Post database models

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,41 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+import forms
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 # Secret key to protect against modifying cookies and cross-site forgery attacks
 app.config['SECRET_KEY'] = "a40fbbb5e9691778bfcf707d3c559c2f"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+
+POST_TITLE_LEN = 100
+
+# Each class inheriting from db.Model is a DB table
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(forms.USR_MAX_LEN), unique=True, nullable=False)
+    email = db.Column(db.String(forms.EMAIL_MAX_LEN), unique=True, nullable=False)
+    password = db.Column(db.String(forms.PASSWD_HASH_LEN), nullable=False)
+    profile_img_file = db.Column(db.String(forms.FILENAME_LEN), nullable=False, default="default.jpg")
+    # Additional query that gets all the user's posts lazily loaded (as they are needed)
+    posts = db.relationship("Post", backref="author", lazy=True) # post.author is linked to User() class
+    # Note: we can do `post.author` but know that this runs a query in the background b/c `author` is not an attribute in the post table
+
+    def __repr__(self):
+        return "User({}, {}, {})".format(self.username, self.email, self.profile_img_file)
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(POST_TITLE_LEN), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # utcnow() func is the param
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False) # primary key in the 'user' table
+
+    def __repr__(self):
+        return "Post({}, {})".format(self.title, self.date_posted)
 
 blogs = [
     {
@@ -92,7 +123,7 @@ def about():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    form = RegistrationForm()
+    form = forms.RegistrationForm()
     if form.validate_on_submit():
         flash("Account for {} successfully created.".format(form.username.data), category="success")
         return redirect(url_for("home"))
@@ -100,7 +131,7 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
+    form = forms.LoginForm()
     if form.validate_on_submit():
         if form.username.data == "adrianmuino" and form.password.data == "asdfghjkl":
             return redirect(url_for("home"))
